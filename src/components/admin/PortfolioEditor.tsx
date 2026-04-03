bb473f9eda54f5c1b1c30bf21b1058032b31511c
Michiel P. <peeraer.michiel@gmail.com>
2026-04-03 13:51:25 +0200

    refactor: simplify layout structure in PortfolioEditor component

--- src/components/admin/PortfolioEditor.tsx
+++ src/components/admin/PortfolioEditor.tsx
@@ -36,40 +36,36 @@ export function PortfolioEditor({ initialData }: PortfolioEditorProps) {
     } = usePortfolioEditorState(initialData)
 
     return (
-        <div className="grid gap-6 xl:grid-cols-[minmax(0,0.95fr)_minmax(0,1.25fr)] items-start">
-            <div className="space-y-6 xl:sticky xl:top-6">
-                <QuickEditSection
-                    register={register}
-                    errors={errors}
-                    isSubmitting={isSubmitting}
-                    formStatus={formStatus}
-                    handleSubmit={handleSubmit}
-                    onSubmit={onSubmitForm}
-                />
-            </div>
+        <div className="space-y-6">
+            <QuickEditSection
+                register={register}
+                errors={errors}
+                isSubmitting={isSubmitting}
+                formStatus={formStatus}
+                handleSubmit={handleSubmit}
+                onSubmit={onSubmitForm}
+            />
 
-            <div className="space-y-6">
-                <SectionsEditorSection
-                    sectionsDraft={sectionsDraft}
-                    setSectionsDraft={setSectionsDraft}
-                    updateExperienceField={updateExperienceField}
-                    updateEducationField={updateEducationField}
-                    updateSkillCategoryField={updateSkillCategoryField}
-                    saveSections={saveSections}
-                    isSavingSections={isSavingSections}
-                    sectionsStatus={sectionsStatus}
-                    sectionIssues={sectionIssues}
-                />
+            <SectionsEditorSection
+                sectionsDraft={sectionsDraft}
+                setSectionsDraft={setSectionsDraft}
+                updateExperienceField={updateExperienceField}
+                updateEducationField={updateEducationField}
+                updateSkillCategoryField={updateSkillCategoryField}
+                saveSections={saveSections}
+                isSavingSections={isSavingSections}
+                sectionsStatus={sectionsStatus}
+                sectionIssues={sectionIssues}
+            />
 
-                <AdvancedJsonSection
-                    value={value}
-                    setValue={setValue}
-                    save={saveJson}
-                    isSaving={isSaving}
-                    rawStatus={rawStatus}
-                    rawIssues={rawIssues}
-                />
-            </div>
+            <AdvancedJsonSection
+                value={value}
+                setValue={setValue}
+                save={saveJson}
+                isSaving={isSaving}
+                rawStatus={rawStatus}
+                rawIssues={rawIssues}
+            />
         </div>
     )
 }
