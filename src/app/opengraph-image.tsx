fb906115ec519ac92d4c36a14cc0ae448cfbb814
Michiel P. <peeraer.michiel@gmail.com>
2026-04-16 18:25:14 +0200

    fix: add line break between LinkedIn and GitHub labels in OpenGraph image component

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -227,6 +227,7 @@ export default async function Image() {
                             }}
                         >
                             {linkedInLabel}
+                            <br />
                             {gitHubLabel}
                         </div>
                     </div>
